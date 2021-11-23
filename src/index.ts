ce160af9bca14d0b7b6ec610cf78e72d93929012
Jonathan Cardenas <jonscardenas@gmail.com>
2021-11-23 16:18:51 -0600

    traditional Js code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello');
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+    // tells fs readFiile Sync what type of file information to expect
+    encoding: 'utf-8'
+}).split('\n')
+.map((row: string): string[] => {
+    return row.split(',');
+});
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    }else if (match[2] === 'Man United' && match[5] === 'A'){
+        manUnitedWins++;
+    }
+}
+
+console.log(`Man United won ${manUnitedWins} games.`)
